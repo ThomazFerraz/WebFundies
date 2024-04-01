d105f00cb285c1768d8c30c7253c8b8d79081c18
projeto do kaike <kaikecosta28@gmail.com>
2024-04-01 20:39:34 -0300

    insercao de JS para clique de botao

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('homepage.html')
+
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
+
+app.static_folder = 'static'
 
 
 if __name__ == '__main__':
